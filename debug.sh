138a9fb09f0fd5dd08f047677f44b923203f9d1a
Alex Rozgo <alex.rozgo@gmail.com>
2019-09-20 21:38:26 -0600

    updated cargo and debug launcher

--- debug.sh
+++ debug.sh
@@ -2,4 +2,16 @@
 
 export GST_PLUGIN_PATH=`pwd`/target/debug
 
-cargo build && gst-launch-1.0 filesrc location=assets/sample.webm ! decodebin ! videoconvert ! videoscale ! tf_segmentation ! video/x-raw,format=RGB,width=512,height=288 ! videoconvert ! ximagesink
+cargo build && \
+GST_DEBUG_DUMP_DOT_DIR=dots \
+GST_TRACERS="debugserver(port=8080)" \
+gst-launch-1.0 \
+    filesrc location=assets/sample.webm ! \
+    decodebin ! videoconvert ! videoscale ! \
+    tf_segmentation ! video/x-raw,format=RGB,width=512,height=288 ! \
+    videoconvert ! ximagesink
+
+# gst-debugger-1.0
+
+# queue ! morpheus_rgb camera=fpv ! video/x-raw,format=RGB,width=512,height=288 ! tf_segmentation
+# queue ! morpheus_depth camera=bottom ! 
